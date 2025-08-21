9289a11ba5aa0719f73611a4b6b4fc197522e030
Charlotte Dann <pouretrebelle@github.com>
2025-08-21 15:45:19 +0100

    :sparkles: Use circular targets for highlights

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,23 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react'
 
+import { drawColorWheel, drawSwatchShape } from 'lib/canvas/colorWheel'
+import { swatches } from 'lib/data/colorWheel'
+import { clamp, map, wrap } from 'lib/format/numbers'
 
-
-import { drawColorWheel, drawSwatchShape } from 'lib/canvas/colorWheel';
-import { swatches } from 'lib/data/colorWheel';
-import { clamp, map, wrap } from 'lib/format/numbers';
-
-
-
-import styles from './Canvas.module.css';
-
+import styles from './Canvas.module.css'
 
 const PIXEL_RATIO = 2
 const WIDTH = 800
 const HEIGHT = 800
 
 type Highlight = {
-  index: number
+  swatchI: number
   level: 1 | 2 | 3
+  x: number
+  y: number
+}
+
+const getHighlightRadius = (h: Highlight) => {
+  return 5 + h.level * 12
 }
 
 export const Canvas: React.FC = () => {
@@ -48,14 +49,27 @@ export const Canvas: React.FC = () => {
 
     // draw highlights
     c.save()
-    highlights.forEach(({ index, level }) => {
-      const swatch = swatches[index]
+    highlights.forEach((h) => {
+      const { swatchI, x, y } = h
+      const swatch = swatches[swatchI]
       if (!swatch) return
 
-      c.strokeStyle = 'black'
-      c.lineWidth = level
-      drawSwatchShape(c, swatch, WIDTH, HEIGHT)
+      c.fillStyle = swatch.colorP3
+      c.strokeStyle = 'hsla(0, 0%, 0%, 0.2)'
+      c.lineWidth = 2
+      c.shadowColor = 'hsla(0, 0%, 0%, 0.5)'
+      c.shadowBlur = 15
+      c.beginPath()
+      c.arc(
+        map(x, -1, 1, 0, WIDTH),
+        map(y, -1, 1, 0, HEIGHT),
+        getHighlightRadius(h),
+        0,
+        2 * Math.PI
+      )
+      c.closePath()
       c.stroke()
+      c.fill()
     })
     c.restore()
 
@@ -63,28 +77,21 @@ export const Canvas: React.FC = () => {
     if (highlights.length > 0) {
       let runningX = 0
       let runningY = 0
-      highlights.forEach(({ index, level }) => {
-        const swatch = swatches[index]
+      highlights.forEach(({ swatchI, level, x, y }) => {
+        const swatch = swatches[swatchI]
         if (!swatch) return
-
-        let midAngle = (swatch.startAngle + swatch.endAngle) / 2
-        if (swatch.endAngle < swatch.startAngle) {
-          // segment wraps around 0
-          midAngle = 0
-        }
-        let midRadius = (swatch.startRadius + swatch.endRadius) / 2
 
         let multiplier = 0.1
         if (level === 2) multiplier = 0.3
         if (level === 3) multiplier = 0.6
 
-        runningX += Math.cos(midAngle) * midRadius * multiplier
-        runningY += Math.sin(midAngle) * midRadius * multiplier
+        runningX += x * multiplier
+        runningY += y * multiplier
       })
 
       const border = 10
       c.save()
-      c.lineWidth = 4
+      c.lineWidth = 3
       c.strokeStyle = 'red'
       c.beginPath()
       c.arc(
@@ -99,6 +106,23 @@ export const Canvas: React.FC = () => {
       c.restore()
     }
   }, [highlights])
+
+  const incrementHighlight = (highlight: Highlight) => {
+    // turn off when at max level
+    if (highlight?.level === 3) {
+      setHighlights(highlights.filter((h) => h.swatchI !== highlight.swatchI))
+    }
+    // increment level
+    else {
+      setHighlights(
+        highlights.map((h) =>
+          h.swatchI === highlight.swatchI
+            ? { ...h, level: (h.level + 1) as 1 | 2 | 3 }
+            : h
+        )
+      )
+    }
+  }
 
   const handleClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
     const canvas = canvasRef.current
@@ -123,39 +147,47 @@ export const Canvas: React.FC = () => {
     const angle = wrap(Math.atan2(y, x), 0, 2 * Math.PI)
     const radius = Math.sqrt(x * x + y * y)
 
+    // check highlight hit
+    if (highlights.length > 0) {
+      // reverse array so top highlight is checked first
+      const hit = highlights
+        .slice()
+        .reverse()
+        .find((h) => {
+          const hitRadius = getHighlightRadius(h)
+          const hitX = map(h.x, -1, 1, 0, WIDTH)
+          const hitY = map(h.y, -1, 1, 0, HEIGHT)
+          const distance = Math.sqrt(
+            (hitX - event.clientX) ** 2 + (hitY - event.clientY) ** 2
+          )
+          return distance <= hitRadius
+        })
+      if (hit) {
+        incrementHighlight(hit)
+        return
+      }
+    }
+
+    // find swatch hit
     const swatch = swatches.find((s) => {
       let hitAngle = s.startAngle <= angle && s.endAngle >= angle
       if (s.endAngle < s.startAngle) {
         // segment wraps around 0
         hitAngle = angle <= s.endAngle || angle >= s.startAngle
       }
-
       let hitRadius = s.startRadius >= radius && s.endRadius <= radius
-
       return hitRadius && hitAngle
     })
 
     if (swatch) {
-      const highlight = highlights.find((h) => h.index === swatch.index)
-      // turn off when at max level
-      if (highlight?.level === 3) {
-        setHighlights(highlights.filter((h) => h.index !== swatch.index))
-      }
-      // increment level
-      else if (highlight) {
-        setHighlights(
-          highlights.map((h) =>
-            h.index === swatch.index
-              ? { ...h, level: ((h.level + 1) as 1 | 2 | 3) }
-              : h
-          )
-        )
-      }
-      // add new highlight
-      else {
+      const highlight = highlights.find((h) => h.swatchI === swatch.index)
+      if (highlight) {
+        incrementHighlight(highlight)
+      } else {
+        // add new highlight
         setHighlights([
           ...highlights,
-          { index: swatch.index, level: 1 },
+          { swatchI: swatch.index, level: 1, x, y },
         ])
       }
     }
